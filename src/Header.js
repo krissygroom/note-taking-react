48c1c190cb74f11eb7a0b8abc8884ad19f3a7a21
krissy <krissydgroom@gmail.com>
2022-01-12 16:08:12 -0800

    Implemented presentational components

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="app-header__title">
+      <h1>Super Sticky Notes</h1>
+      <aside className="app-header__controls">
+        <button className="add-new">+ New Note</button>
+        <input
+          className="search"
+          type="text"
+          placeholder="Type here to search..."
+        />
+      </aside>
+    </header>
+  );
+};
+
+export default Header;
